8322df6374a9f5d57f3c1175b734c31537838b04
abdullahjr0 <abdullah.rafique01@gmail.com>
2022-01-05 12:13:59 +0000

    adjust run.py

--- run.py
+++ run.py
@@ -18,14 +18,60 @@ def category():
     return render_template("category.html")
 
 @app.route("/contact")
+@login_required
 def contact():
-    return render_template("contact.html")
+    return render_template("FeedBackForm.html")
 
-@app.route("/sign")
-def sign():
-    return render_template("sign.html")
+@app.route("/register", methods=["POST","GET"])
+def register():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        check = User.query.filter_by(email=email).first()
+        if check:
+            flash("Email Address already exists , try different one")
+        else:
+            try:
+                user = User(
+                    name=name,
+                    email = email,
+                    password = password
+                )
+                db.session.add(user)
+                db.session.commit()
+                login_user(user)
+                flash('Register succesfully')
+                return redirect('/profile')
+            except:
+                flash("Problem in Registration")
+                return redirect("/register")
+    return render_template("Sign-up.html")
 
+@app.route("/login", methods=["GET","POST"])
+def login():
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            user = User.query.filter_by(email=email).first()
+            try:
+                if password == user.password:
+                    login_user(user)
+                    return redirect('/')
+                else:
+                    flash("Wrong password")
+                    return redirect('/login')
+            except:
+                flash("Wrong Email Address")
+                return redirect('/login')
+        return render_template('/signin.html')
 
+@app.route("/contact_us", methods=["GET","POST"])
+@login_required
+def contact_us():
+    message = "yes"
+    return render_template("FeedBackForm.html", message = message)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        
+    if __name__ == "__main__":
+        app.run(debug=True)
